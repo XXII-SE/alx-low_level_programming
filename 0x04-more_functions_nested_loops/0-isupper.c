b7d1e68d506c5c8fcefe0ab98a16b9428252a563
Victor Kenneth <victorkenneth.vk@gmail.com>
2021-06-24 13:09:58 -0700

    task 0

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,4 +1,3 @@
-
 /**
  * _isupper - checks for uppercase letters
  * @c: character to check
@@ -9,8 +8,8 @@
 
 int _isupper(int c)
 {
-	if (c >= 'A' && c <= 'Z')
-		return (1);
-	else
-		return (0);
+        if (c >= 'A' && c <= 'Z')
+                return (1);
+        else
+                return (0);
 }
